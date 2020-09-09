aca8f57be96abc253a2b5b783131962941f327bb
Carlos Montalvo <cmontalvo251@gmail.com>
2020-09-09 13:11:18 -0500

    Added HIL_Single_Number in test scripts

--- Test_Scripts/HIL_Send_Single_Number/main.c
+++ Test_Scripts/HIL_Send_Single_Number/main.c
@@ -14,7 +14,7 @@ int main() {
 	printf("Sending w slash r \n");
 	SerialPutc(&my,'w');
 	SerialPutc(&my,'\r');
-	//printf("Sent \n");
+	printf("Sent \n");
 
 	//Consume w\r\n
 	printf("Reading the Serial Buffer for w slash r slash n \n");
@@ -23,4 +23,22 @@ int main() {
 	  inchar = SerialGetc(&my);
 	  printf("%d \n",int(inchar));
 	}
+
+	//Create fictitious float
+	float number = 5.0;
+	float number_array[MAXFLOATS];
+	number_array[0] = number;
+	int number_of_numbers = 1;
+
+	//Send to Arduino
+	SerialPutArray(&my,number_array,number_of_numbers);
+
+	//Now Read from Arduino
+	SerialGetArray(&my,number_array,number_of_numbers);
+
+	//Extract Data
+	//float rec_number = number_array[0];
+
+	//printf("Number Received = %lf \n",rec_number);
+
 } //end main loop desktop computer
