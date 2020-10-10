37411c135548cecba084ec6032e782ad3137f25e
MJCobar <maxwell.cobar@gmail.com>
2020-10-10 01:37:13 -0500

    Added the telemetry files that run on the laptop.

---
+++ Test_Scripts/Telemetry_Maxwell/Telemetry_Read_Computer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import serial
+from serial import Serial
+import time
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1)
+    ser.flush()
+    
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
