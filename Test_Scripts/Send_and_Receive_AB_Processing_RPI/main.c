3701540da244e3f1cc3c5e07e825932c71d50f89
MJCobar <maxwell.cobar@gmail.com>
2020-10-12 18:19:31 +0000

    Overhauled Serial library for RPi and it kinda works.

--- Test_Scripts/Send_and_Receive_AB_Processing_RPI/main.c
+++ Test_Scripts/Send_and_Receive_AB_Processing_RPI/main.c
@@ -10,9 +10,6 @@ int main() {
   //using this baudrate
   printf("Initializing the dev Port \n");
   my = SerialInit("/dev/ttyAMA0",57600);  //This is the UART port on the RPI
-  if (my < 0) {
-    printf("Error Opening Dev Port \n");
-  }
   printf("Dev Port Initialized. If no errors present we are currently listening \n");
   //sleep(5); //--This was an arduino hack. I don't think we need it.
 
@@ -21,24 +18,29 @@ int main() {
 
   while (1) {
     //From here we basically need to constantly read the serial port at least once
-    //in the while loop and check for w\r from the computer so I'll need to write a
-    //int ok = SerialListen(&my,1); //set to 0 to turn echo off, 1 = all echos on, 2 = only echo if you receive anything
+    //in the while loop and check for w\r from the computer so I'll need to write
+
+    /////////////////THIS WORKS SO DON"T TOUCH
+    /*char dat;
+    if(serialDataAvail(my))
+      {
+	dat = serialGetchar(my);
+	printf("char = %c int(char) = %d \n", dat,int(dat));
+	}*/
+    //////////////////////////////////////////
+
+    int ok = SerialListen(&my,0); //set to 0 to turn echo off, 1 = all echos on, 2 = only echo if you receive anything
     // w\r was received it means we need to responde
-
-    ///DEBUGGING
-    char inchar;
-    int val;
-    if (serialDataAvail(my)) {
-      inchar = SerialGetc(&my);
-      val = int(inchar);
-      printf("Character Received = %c ASCII Code = %d \n",inchar,val);
-    }
     
-    //if (ok == (119+13)) { //119 is ASCII for w and 13 is ASCII for \r
-    //printf("w slash r received!! \n");
+    if (ok == (119+13)) { //119 is ASCII for w and 13 is ASCII for \r
+      printf("w slash r received!! \n");
+    //}
 
-      /*
+      sleep(1);
+      
+      ///THIS DOES NOT WORK BUT SENDAB TO PROCESSING DOES WORK SO TRY THAT CODE FIRST
       SerialRespond(&my,1);
+      /*
       //Once we've responded we must send whomever is talking to us some data
 
       //Create fictitious floats
@@ -51,9 +53,7 @@ int main() {
       //Send over Serial
       SerialPutArray(&my,number_array,number_of_numbers);
       */
-    //}
-  
-    
-    
+    }
+      
   } //end robot while loop
 } //end main loop robot
