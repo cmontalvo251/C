3cd2b3e4b8574421ab809170426f0a05c732009a
Carlos Montalvo <cmontalvo@southalabama.edu>
2021-07-11 12:30:17 -0500

    Added some more test code to the telemetry software

--- Test_Scripts/Telemetry_Class/main.cpp
+++ Test_Scripts/Telemetry_Class/main.cpp
@@ -10,5 +10,14 @@ int main(int argc, char** argv) {
 	serial.SerialInit("/dev/ttyACM0",115200);
 	printf("If no errors present, serial port is open \n");
 	serial.SerialPutHello();
+	float number_array[7];
+	number_array[0] = 3.4;
+	number_array[1] = -2.3;
+	number_array[2] = 0.4;
+	number_array[3] = -0.1;
+	number_array[4] = 5.8;
+	number_array[5] = 300.0;
+	number_array[6] = -300.0;
+	serial.SerialSendArray(number_array,7);
 	return 0;
 }
